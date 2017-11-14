752287d3687dacf8b2e65869cbf146a1a47af8bb
luisf11 <luisfernandop1110@gmail.com>
2017-11-14 13:35:34 -0400

    Add scroll horizonal

--- App.js
+++ App.js
@@ -6,7 +6,8 @@ import {
   View,
   StatusBar,
   Image,
-  FlatList
+  FlatList,
+  ScrollView
 } from "react-native";
 import Heart from "./assets/icons/heart";
 import LikeIcon from "./assets/icons/like.icon";
@@ -14,32 +15,32 @@ import LikeIcon from "./assets/icons/like.icon";
 const data = [
   {
     key: 1,
-    imageUrl: "http://via.placeholder.com/300x300",
+    imageUrl: "http://via.placeholder.com/300x400",
     title: "something"
   },
   {
     key: 2,
-    imageUrl: "http://via.placeholder.com/300x300",
+    imageUrl: "http://via.placeholder.com/300x400",
     title: "something two"
   },
   {
     key: 3,
-    imageUrl: "http://via.placeholder.com/300x300",
+    imageUrl: "http://via.placeholder.com/300x400",
     title: "something three"
   },
   {
     key: 4,
-    imageUrl: "http://via.placeholder.com/300x300",
+    imageUrl: "http://via.placeholder.com/300x400",
     title: "something four"
   },
   {
     key: 5,
-    imageUrl: "http://via.placeholder.com/300x300",
+    imageUrl: "http://via.placeholder.com/300x400",
     title: "something five"
   },
   {
     key: 6,
-    imageUrl: "http://via.placeholder.com/300x300",
+    imageUrl: "http://via.placeholder.com/300x400",
     title: "something six"
   }
 ];
@@ -65,19 +66,31 @@ export default class App extends React.Component {
                 </View>
               </View>
               <View style={styles.scrollViewWrapper}>
-               <FlatList
-                horizontal
-                data={this.state.data}
-                renderItem = {({item: rowData}) => {
-                  return(
-                    <Image style={{flex:1}}  source={{uri: rowData.imageUrl }}/>
-                  );
-                }}
-               />
+                <ScrollView
+                  contentContainerStyle={styles.contentContainer}
+                  horizontal={true}
+                  decelerationRate={0}
+                  showsHorizontalScrollIndicator={false}
+                  pagingEnabled={true}
+                  snapToInterval={300}
+       >
+                  {
+                    this.state.data.map((item) => (
+                    <Image key={item.key} source={{uri: item.imageUrl}} style={styles.image}/>
+                  ))
+                  }
+
+
+                </ScrollView>
               </View>
             </View>
             <View style={styles.cardFooterContainer}>
-              <Text>footer</Text>
+              <View style={styles.footerCenter}>  
+                <Text>footer center container</Text>
+              </View>
+              <View style={styles.footerRight}>  
+                <Text>Rigth content</Text>
+              </View>
             </View>
           </View>
         </View>
@@ -102,7 +115,7 @@ const styles = StyleSheet.create({
     flexDirection: "column"
   },
   card: {
-    backgroundColor: "blue",
+   
     flex: 1,
     flexDirection: "column"
   },
@@ -122,14 +135,42 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 3
   },
+  image: {
+    width:400,
+    height:300
+  },
   cardFooterContainer: {
     flex: 1,
-    backgroundColor: "yellow"
+    backgroundColor: "yellow",
+    flexDirection: 'row'
+  },
+  footerCenter:{
+    alignItems: "flex-start",
+    flex:2,
+    marginTop:5,
+    marginBottom:5,
+    marginLeft:10,
+    marginRight:5,
+    backgroundColor: "blue",
+  },
+  footerRight:{
+    marginTop:5,
+    marginBottom:5,
+    marginRight:5,
+    alignItems: "flex-start",
+    flex:1,
+    backgroundColor: "red",
   },
   scrollViewWrapper:{
     width: 400,
     height: 300,
 
-  }
+  },
+  contentContainer: {
+    borderStyle: "solid",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+
 
 });
